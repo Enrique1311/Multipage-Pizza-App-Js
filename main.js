46c3b9291e19bd737424257b1f64ecc2342a4bb6
Enrique1311 <ejspinelli131176@gmail.com>
2024-07-24 18:10:50 -0300

    The mobile menu has been added.

--- main.js
+++ main.js
@@ -5,16 +5,29 @@ const d = document,
 const $loginIcon = d.querySelector(".login-icon i"),
 	$loginForm = d.querySelector(".login-form");
 
+const $menuIcon = d.querySelector(".menu-icon"),
+	$mobileNav = d.querySelector(".mobile-nav");
+
 // Shopping Cart ******************************
 
 $cartIcon.addEventListener("click", () => {
 	$cartContainer.classList.toggle("active");
 	$loginForm.classList.remove("active");
+	$mobileNav.classList.remove("active");
 });
 
 // Login Form ******************************
 
 $loginIcon.addEventListener("click", () => {
 	$loginForm.classList.toggle("active");
+	$cartContainer.classList.remove("active");
+	$mobileNav.classList.remove("active");
+});
+
+// Mobile Menu ****************************
+
+$menuIcon.addEventListener("click", () => {
+	$mobileNav.classList.toggle("active");
+	$loginForm.classList.remove("active");
 	$cartContainer.classList.remove("active");
 });
