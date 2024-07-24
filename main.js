3fa09e4ad8005b5e4572f933173e519f29aaa868
Enrique1311 <ejspinelli131176@gmail.com>
2024-07-23 23:56:32 -0300

    The login form has been added

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ const d = document,
 	$cartIcon = d.querySelector(".cart-icon i"),
 	$cartContainer = d.querySelector(".cart-container");
 
-console.log($cartIcon, $cartContainer);
+const $loginIcon = d.querySelector(".login-icon i"),
+	$loginForm = d.querySelector(".login-form");
+
+// Shopping Cart ******************************
 
 $cartIcon.addEventListener("click", () => {
 	$cartContainer.classList.toggle("active");
+	$loginForm.classList.remove("active");
+});
+
+// Login Form ******************************
+
+$loginIcon.addEventListener("click", () => {
+	$loginForm.classList.toggle("active");
+	$cartContainer.classList.remove("active");
 });
