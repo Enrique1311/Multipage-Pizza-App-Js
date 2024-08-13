2eac98b755ced027060335ab683f05ede3c01d3f
Enrique1311 <ejspinelli131176@gmail.com>
2024-08-12 22:16:40 -0300

    The menu was ended

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ import { pageHeading } from "../helpers/createPageHeading.js";
 import { createFooter } from "../helpers/createFooter.js";
 
 const d = document;
+const $menu = d.querySelector(".menu");
 
 // Header *******************************************
 createHeader();
@@ -14,7 +15,25 @@ pageHeading("Menu");
 createFooter();
 
 // Menu Page Content ********************************
-const $menu = d.querySelector(".menu");
+
+const $pizzasContent = d.getElementById("pizzas");
+const $pastasContent = d.getElementById("pastas");
+const $dessertsContent = d.getElementById("desserts");
+const $winesContent = d.getElementById("wines");
+
+const writeMenuItems = (el, content) => {
+	content.innerHTML += `
+		<article class="menu-item">
+			<div class="menu-item-name">
+				<h3>${el.name}</h3>
+				<p>${el.ingredients}</p>
+			</div>
+			<div class="item-price">
+				<h2>$${el.price}</h2>
+			</div>
+		</article>
+`;
+};
 
 const getMenu = async () => {
 	try {
@@ -25,8 +44,20 @@ const getMenu = async () => {
 
 		if (!res.ok) throw { statusText: res.statusText };
 
-		data.forEach((el) => {
-			s;
+		data.pizzas.forEach((el) => {
+			writeMenuItems(el, $pizzasContent);
+		});
+
+		data.pastas.forEach((el) => {
+			writeMenuItems(el, $pastasContent);
+		});
+
+		data.desserts.forEach((el) => {
+			writeMenuItems(el, $dessertsContent);
+		});
+
+		data.wines.forEach((el) => {
+			writeMenuItems(el, $winesContent);
 		});
 	} catch (err) {
 		let message = err.statusText || "Not Found";
